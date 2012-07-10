17caf02dc4d19be348196a6bc870f7aa46d7f213
Roberto Bonvallet <rbonvall@gmail.com>
2012-07-10 12:56:40 -0400

    k

---
+++ src/cublas.cu
@@ -0,0 +1,7 @@
+void matrix_vector_product(int nr_rows, int nr_cols,
+                           float *b, float *a, float *x) {
+    cublasSgemv('t', nr_cols, nr_rows,
+      1.0f, a, nr_cols,
+            x, 1,
+      0.0f, b, 1);
+}
