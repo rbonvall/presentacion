b52f10e2aa884a76ab4cefee83bafdf3df4b9384
Roberto Bonvallet <rbonvall@gmail.com>
2012-07-10 13:06:26 -0400

    x

--- src/cublas.cu
+++ src/cublas.cu
@@ -1,5 +1,7 @@
 void matrix_vector_product(int nr_rows, int nr_cols,
-                           float *b, float *a, float *x) {
+                           float *b,
+                           float *a,
+                           float *x) {
     cublasSgemv('t', nr_cols, nr_rows,
       1.0f, a, nr_cols,
             x, 1,
